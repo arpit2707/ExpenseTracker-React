4615e79fbe550b40afec2959643abbeaa3934ecb
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-10-09 22:58:49 +0530

    renderingList Of Expense

--- expensetracker/src/components/Expenses/Expenses.js
+++ expensetracker/src/components/Expenses/Expenses.js
@@ -6,30 +6,14 @@ import "./Expenses.css";
 const Expenses = (props) => {
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-        location={props.items[0].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-        location={props.items[1].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-        location={props.items[2].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        location={props.items[3].locationOfExpenditure}
-      />
+      {props.items.map((item) => (
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+          location={item.locationOfExpenditure}
+        />
+      ))}
     </Card>
   );
 };
